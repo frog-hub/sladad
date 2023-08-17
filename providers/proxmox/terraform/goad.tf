2f5550ad62d4244946d3eaf5c49770c35e95ea7b
frog-hub <127029763+frog-hub@users.noreply.github.com>
2023-08-17 22:24:18 +0200

    Update Domains and IP

--- providers/proxmox/terraform/goad.tf
+++ providers/proxmox/terraform/goad.tf
@@ -1,6 +1,6 @@
 resource "proxmox_vm_qemu" "dc01" {
     name = "DC01"
-    desc = "DC01 - windows server 2019 - 192.168.10.10"
+    desc = "DC01 - windows server 2019 - 172.16.10.10"
     qemu_os = "win10"
     target_node = var.pm_node
     pool = var.pm_pool
@@ -12,7 +12,7 @@ resource "proxmox_vm_qemu" "dc01" {
     clone = "WinServer2019x64-cloudinit-qcow2"
 
    network {
-     bridge    = "vmbr3"
+     bridge    = "vmbr2"
      model     = "e1000"
      tag       = 10
    }
@@ -21,13 +21,13 @@ resource "proxmox_vm_qemu" "dc01" {
         disk,
       ]
     }
-   nameserver = "192.168.10.1"
-   ipconfig0 = "ip=192.168.10.10/24,gw=192.168.10.1"
+   nameserver = "172.16.100.1"
+   ipconfig0 = "ip=172.16.100.10/24,gw=172.16.100.254"
 }
 
 resource "proxmox_vm_qemu" "dc02" {
     name = "DC02"
-    desc = "DC02 - windows server 2019 - 192.168.10.11"
+    desc = "DC02 - windows server 2019 - 172.16.100.11"
     qemu_os = "win10"
     target_node = var.pm_node
     pool = var.pm_pool
@@ -37,7 +37,7 @@ resource "proxmox_vm_qemu" "dc02" {
     agent = 1
     clone = "WinServer2019x64-cloudinit-qcow2"
     network {
-      bridge    = "vmbr3"
+      bridge    = "vmbr2"
       model     = "e1000"
       tag       = 10
     }
@@ -46,13 +46,13 @@ resource "proxmox_vm_qemu" "dc02" {
         disk,
       ]
     }
-   nameserver = "192.168.10.1"
-   ipconfig0 = "ip=192.168.10.11/24,gw=192.168.10.1"
+   nameserver = "172.16.100.1"
+   ipconfig0 = "ip=172.16.100.11/24,gw=172.16.100.254"
 }
 
 resource "proxmox_vm_qemu" "DC03" {
     name = "DC03"
-    desc = "DC03 - windows server 2016 - 192.168.10.12"
+    desc = "DC03 - windows server 2016 - 172.16.100.12"
     qemu_os = "win10"
 
     target_node = var.pm_node
@@ -65,22 +65,22 @@ resource "proxmox_vm_qemu" "DC03" {
     clone = "WinServer2016x64-cloudinit-qcow2"
 
     network {
-      bridge    = "vmbr3"
+      bridge    = "vmbr2"
       model     = "e1000"
-      tag       = 10 
+      tag       = 10
     }
     lifecycle {
       ignore_changes = [
         disk,
       ]
     }
-   nameserver = "192.168.10.1"
-   ipconfig0 = "ip=192.168.10.12/24,gw=192.168.10.1"
+   nameserver = "172.16.100.1"
+   ipconfig0 = "ip=172.16.100.12/24,gw=172.16.100.254"
 }
 
 resource "proxmox_vm_qemu" "srv02" {
     name = "SRV02"
-    desc = "SRV02 - windows server 2019 - 192.168.10.22"
+    desc = "SRV02 - windows server 2019 - 172.16.100.22"
     qemu_os = "win10"
     target_node = var.pm_node
     pool = var.pm_pool
@@ -91,7 +91,7 @@ resource "proxmox_vm_qemu" "srv02" {
     clone = "WinServer2019x64-cloudinit-qcow2"
 
     network {
-      bridge    = "vmbr3"
+      bridge    = "vmbr2"
       model     = "e1000"
       tag       = 10
     }
@@ -100,13 +100,13 @@ resource "proxmox_vm_qemu" "srv02" {
         disk,
       ]
     }
-    nameserver = "192.168.10.1"
-    ipconfig0 = "ip=192.168.10.22/24,gw=192.168.10.1"
+    nameserver = "172.16.100.1"
+    ipconfig0 = "ip=172.16.100.22/24,gw=172.16.100.254"
 }
 
 resource "proxmox_vm_qemu" "srv03" {
     name = "SRV03"
-    desc = "SRV03 - windows server 2016 - 192.168.10.23"
+    desc = "SRV03 - windows server 2016 - 172.16.100.23"
     qemu_os = "win10"
     target_node = var.pm_node
     pool = var.pm_pool
@@ -117,16 +117,16 @@ resource "proxmox_vm_qemu" "srv03" {
     clone = "WinServer2016x64-cloudinit-qcow2"
 
     network {
-      bridge    = "vmbr3"
+      bridge    = "vmbr2"
       model     = "e1000"
       tag       = 10
     }
-    
+
     lifecycle {
       ignore_changes = [
         disk,
       ]
     }
-    nameserver = "192.168.10.1"
-    ipconfig0 = "ip=192.168.10.23/24,gw=192.168.10.1"
+    nameserver = "172.16.100.1"
+    ipconfig0 = "ip=172.16.100.23/24,gw=172.16.100.254"
 }
